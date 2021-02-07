d92a868a4ee0077ded4741bb15679fe1a37167bd
ac349 <76962237+ac349@users.noreply.github.com>
2021-02-07 18:17:23 -0500

    Add files via upload

---
+++ experiment 1 graph.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as pp
+import numpy as np
+
+temp_list = [30.7, 30.6, 30.6, 30.6, 30.6]
+humidity_list = [41.9, 41.9, 41.9, 41.9, 41.9]
+
+t = np.arange(0,5,1)
+y1 = temp_list
+y2 =humidity_list
+
+fig, ax1 = pp.subplots()
+ax1.set_xlabel('time (h)')
+ax1.set_ylabel('temperature (°c)')
+ax1.plot(t,y1, color = "red")
+ax1.tick_params(axis = 'y') 
+
+ax2 = ax1.twinx()
+ax2.set_ylabel("humidity (%)")
+ax2.plot(t, y2, color = "blue")
+ax2.tick_params(axis='y')
+fig.tight_layout()
+pp.show()
---
+++ realdigitfinder.py
@@ -0,0 +1,16 @@
+import re
+
+def parse_file(filename):
+    digit_pattern = re.compile(r"(\d+\.\d+)")
+    temperatures= []
+    humidities = []
+    with open(filename) as input_file:
+        for input_line in input_file.read().splitlines():
+            results = digit_pattern.findall(input_line)
+            temperatures.append(float(results[0]))
+            humidities.append(float(results[1]))
+        return temperatures, humidities
+            
+returned_temperatures, returned_humidities = parse_file("experiment_1_data.txt")
+print("temperatures=",returned_temperatures)
+print("humidities=", returned_humidities)
